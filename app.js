a5d963a1f773d8b77ba6bd56ed7ee13d6ec8810a
shasheeshpurohit@outlook.com <shasheesh15@users.noreply.github.com>
2020-12-08 09:46:44 +0530

    Taking input

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
+var txtArea = document.querySelector("#txt-area");
+
 btnTranslate.addEventListener("click", () => {
         console.log("I got clicked!");
+        console.log(txtArea.value)
     });
+
+
